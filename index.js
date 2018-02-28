9b10727490fb1bb8769049c40eda8492b04a79ca
Julia Biro <biro.julia.izabella@gmail.com>
2018-02-28 19:51:29 +0000

    Record a voice message

--- index.js
+++ index.js
@@ -1,61 +1,88 @@
-require('dotenv').config({
-  path: __dirname + '/.env'
+require("dotenv").config({
+  path: __dirname + "/.env"
 });
 var config = {
-  API_KEY: process.env.API_KEY || '',
-  API_SECRET: process.env.API_SECRET || '',
-  TO_NUMBER: process.env.TO_NUMBER || '',
-  MEDIA_ID: process.env.MEDIA_ID || '',
-  APP_ID: process.env.APP_ID || '',
-  PRIVATE_KEY: process.env.PRIVATE_KEY || '',
-  SERVER: process.env.SERVER || '',
-  DEBUG: process.env.DEBUG === 'true'
+  API_KEY: process.env.API_KEY || "",
+  API_SECRET: process.env.API_SECRET || "",
+  TO_NUMBER: process.env.TO_NUMBER || "",
+  MEDIA_ID: process.env.MEDIA_ID || "",
+  APP_ID: process.env.APP_ID || "",
+  PRIVATE_KEY: process.env.PRIVATE_KEY || "",
+  SERVER: process.env.SERVER || "",
+  DEBUG: process.env.DEBUG === "true"
 };
 module.exports = config;
 
-const Nexmo = require('nexmo');
+const Nexmo = require("nexmo");
 const nexmo = new Nexmo({
   apiKey: config.API_KEY,
   apiSecret: config.API_SECRET
 });
 var STATUS = "available";
-const app = require('express')();
-const bodyParser = require('body-parser');
+const app = require("express")();
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
 });
 
-app.get('/answer', function(req, res) {
-  const ncco = [{
-      "action": "talk",
-      "text": "Please wait while we connect you."
-    },
-    {
-      "action": "connect",
-      "eventUrl": ["config.SERVER/event"],
-      "from": req.body.from,
-      "endpoint": [{
-        "type": "phone",
-        "number": config.TO_NUMBER
-      }]
-    }
-  ];
-  res.json(ncco);
+app.get("/answer", function(req, res) {
+  if (STATUS.toLowerCase() === "available") {
+    const ncco = [{
+        "action": "talk",
+        "text": "Please wait while we connect you."
+      },
+      {
+        "action": "connect",
+        "eventUrl": [config.SERVER + "/event"],
+        "from": req.body.from,
+        "endpoint": [{
+          "type": "phone",
+          "number": config.TO_NUMBER
+        }]
+      }
+    ];
+    res.json(ncco);
+  } else {
+    const ncco = [{
+        "action": "talk",
+        "voiceName": "Jennifer",
+        "text": "Hi, " + STATUS + "Please leave your name and quick message after the tone, then press #."
+      },
+      {
+        "action": "record",
+        "eventUrl": [config.SERVER + "/record"],
+        "endOnSilence": "3",
+        "endOnKey": "#",
+        "beepStart": "true"
+      },
+      {
+        "action": "talk",
+        "voiceName": "Jennifer",
+        "text": "Thank you for your message. Bye!"
+      }
+    ];
+    res.json(ncco);
+  }
 });
 
-app.post('/event', function(req, res) {
+app.post("/record", (req, res) => {
+console.log(req.body);
+res.status(204).end();
+});
+
+app.post("/event", function(req, res) {
   console.log(req.body);
   res.status(204).end();
 });
 
-app.post('/sms', (req, res) => {
+app.post("/sms", (req, res) => {
   console.log(req.body);
 
-  if (req.body.msisdn == config.TO_NUMBER) {
+  if (req.body.msisdn === config.TO_NUMBER) {
     STATUS = req.body.text;
     console.log(req.body.text);
     res.status(200).end();
